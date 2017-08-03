3a66cafe903c7191980037dbb19c60081c806692
Andrew Boychuk <andrewboychuk@gmail.com>
2017-08-03 18:34:27 +0300

    ABUserCell -> empty methods deleted.

--- Source/UI/ABUsersViewController/ABUserCell/ABUserCell.m
+++ Source/UI/ABUsersViewController/ABUserCell/ABUserCell.m
@@ -21,14 +21,6 @@
 #pragma mark -
 #pragma mark Initializations and Deallocations
 
-- (instancetype)initWithCoder:(NSCoder *)aDecoder {
-    return [super initWithCoder:aDecoder];
-}
-
-- (void)encodeWithCoder:(NSCoder *)aCoder {
-    [super encodeWithCoder:aCoder];
-}
-
 - (void)awakeFromNib {
     [super awakeFromNib];
 }
