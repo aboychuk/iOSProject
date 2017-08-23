bea644562c97baed4eda56e027df5160a2616d0b
Andrew Boychuk <andrewboychuk@gmail.com>
2017-08-23 13:44:35 +0300

    ABConstants added.

---
+++ Source/Constants/ABConstants.h
@@ -0,0 +1,20 @@
+//
+//  ABConstants.h
+//  iOSProject
+//
+//  Created by Andrew Boychuk on 8/23/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+static const UIViewAutoresizing ABAutoresizeWithFixedPosition = UIViewAutoresizingFlexibleLeftMargin
+                                                                | UIViewAutoresizingFlexibleWidth
+                                                                | UIViewAutoresizingFlexibleRightMargin
+                                                                | UIViewAutoresizingFlexibleTopMargin
+                                                                | UIViewAutoresizingFlexibleHeight
+                                                                | UIViewAutoresizingFlexibleBottomMargin;
+
+
+static const UIViewAutoresizing ABAutoresize = UIViewAutoresizingFlexibleWidth
+                                                | UIViewAutoresizingFlexibleHeight;
