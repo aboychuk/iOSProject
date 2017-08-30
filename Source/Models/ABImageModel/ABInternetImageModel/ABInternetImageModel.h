1131ce106833c2f81f7a16abb66f9bb4d92e9c7f
Andrew Boychuk <andrewboychuk@gmail.com>
2017-08-30 18:46:51 +0300

    ABInternetImageModel -> added.

---
+++ Source/Models/ABImageModel/ABInternetImageModel/ABInternetImageModel.h
@@ -0,0 +1,13 @@
+//
+//  ABInternetImageModel.h
+//  iOSProject
+//
+//  Created by Andrew Boychuk on 8/30/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "ABImageModel.h"
+
+@interface ABInternetImageModel : ABImageModel
+
+@end
---
+++ Source/Models/ABImageModel/ABInternetImageModel/ABInternetImageModel.m
@@ -0,0 +1,13 @@
+//
+//  ABInternetImageModel.m
+//  iOSProject
+//
+//  Created by Andrew Boychuk on 8/30/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "ABInternetImageModel.h"
+
+@implementation ABInternetImageModel
+
+@end
