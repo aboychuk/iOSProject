db5534c27240b3ae40bed96b84f14a9cc576a9a2
Andrew Boychuk <andrewboychuk@gmail.com>
2017-09-01 12:54:51 +0300

    ABRandomNumber -> added.

---
+++ Libriary/Classes/ABRandomNumber/ABRandomNumber.h
@@ -0,0 +1,14 @@
+//
+//  ABRandomNumber.h
+//  iOSProject
+//
+//  Created by Andrew Boychuk on 9/1/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+BOOL ABRandomBool();
+NSRange ABMakeRange(NSUInteger firstNumber, NSUInteger lastNumber);
+NSUInteger ABRandomValueInRange(NSRange range);
+NSUInteger ABRandomWithMaxValue(NSUInteger value);
