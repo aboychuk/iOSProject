cfda75da4937b455383b8e0e9787feef46b419d7
Andrew Boychuk <andrewboychuk@gmail.com>
2017-09-14 13:32:02 +0300

    Facebook SDK added.

---
+++ Source/UI/ABUsersViewController/ABUserCell/ABUserCell.m
@@ -0,0 +1,51 @@
+//
+//  ABUserCell.m
+//  iOSProject
+//
+//  Created by Andrew Boychuk on 20.07.17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "ABUserCell.h"
+
+#import "ABUser.h"
+#import "ABImageView.h"
+#import "ABImageModel.h"
+
+@interface ABUserCell ()
+
+- (void)fillWithModel:(ABUser *)user;
+
+@end
+
+@implementation ABUserCell
+
+#pragma mark - 
+#pragma mark Accessors
+
+- (void)setUser:(ABUser *)user {
+    if (_user != user) {
+        _user = user;
+        
+        [self fillWithModel:user];
+    }
+}
+
+#pragma mark -
+#pragma mark Public Methods
+
+- (void)fillWithModel:(ABUser *)user {
+    self.fullNameLabel.text = user.fullname;
+    self.userImageView.imageModel = user.imageModel;
+}
+
+#pragma mark -
+#pragma mark Overriden Methods
+
+- (void)prepareForReuse {
+    [super prepareForReuse];
+    
+    self.userImageView.contentImageView.image = nil;
+}
+
+@end
