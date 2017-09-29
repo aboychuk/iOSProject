614ce8c678e169c94736c2d0671591b37e300550
Andrew Boychuk <andrewboychuk@gmail.com>
2017-09-29 20:37:33 +0300

    ABLoginViewController -> refactored.

--- Source/UI/Facebook/ABLoginViewController/ABLoginViewController.m
+++ Source/UI/Facebook/ABLoginViewController/ABLoginViewController.m
@@ -6,25 +6,70 @@
 //  Copyright © 2017 Andrew Boychuk. All rights reserved.
 //
 
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <FBSDKLoginKit/FBSDKLoginKit.h>
-
 #import "ABLoginViewController.h"
 
 #import "ABLoginView.h"
 #import "ABUserDetailViewController.h"
+#import "ABModel.h"
+#import "ABContext.h"
+#import "ABLoginContext.h"
+#import "ABUser.h"
 
 #import "ABMacro.h"
+
+@interface ABLoginViewController ()
+@property (nonatomic, strong)   ABModel     *model;
+@property (nonatomic, strong)   ABContext   *context;
+
+@end
 
 ABViewControllerRootViewProperty(ABLoginViewController, rootView, ABLoginView)
 
 @implementation ABLoginViewController
 
 #pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.model = nil;
+    self.context = nil;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.model = [ABUser new];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (void)setModel:(ABModel *)model {
+    if (_model != model) {
+        [_model removeObserver:self];
+        
+        _model = model;
+        [_model addObserver:self];
+    }
+}
+
+- (void)setContext:(ABContext *)context {
+    if (_context != context) {
+        [_context cancel];
+        
+        _context = context;
+        [_context execute];
+    }
+}
+
+#pragma mark -
 #pragma mark Actions
 
 - (IBAction)onLogin:(UIButton *)sender {
-    [self login];
+    self.context = [ABLoginContext contextWithModel:self.model];
 }
 
 #pragma mark -
@@ -32,27 +77,16 @@ ABViewControllerRootViewProperty(ABLoginViewController, rootView, ABLoginView)
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if ([FBSDKAccessToken currentAccessToken]) {
+        [self showUserDetailViewController];
+    }
 }
 
 #pragma mark -
 #pragma mark Private
 
-- (void)login {
-    FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
-    [login logInWithReadPermissions: @[@"public_profile"]
-                 fromViewController:self
-                            handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
-                                if (error) {
-                                    NSLog(@"Process error");
-                                } else if (result.isCancelled) {
-                                    NSLog(@"Cancelled");
-                                } else {
-                                    NSLog(@"Logged in");
-                                }
-                            }];
-        if ([FBSDKAccessToken currentAccessToken]) {
-        [self.navigationController pushViewController:[ABUserDetailViewController new] animated:YES];
-    }
+- (void)showUserDetailViewController {
+    [self.navigationController pushViewController:[ABUserDetailViewController new] animated:YES];
 }
 
 @end
