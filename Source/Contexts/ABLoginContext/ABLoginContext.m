df75fd40df764e9cfd699ad50314fcd5de303faa
Andrew Boychuk <andrewboychuk@gmail.com>
2017-09-29 20:36:36 +0300

    ABLoginContext -> login context implemented.

--- Source/Contexts/ABLoginContext/ABLoginContext.m
+++ Source/Contexts/ABLoginContext/ABLoginContext.m
@@ -8,6 +8,34 @@
 
 #import "ABLoginContext.h"
 
+@interface ABLoginContext ()
+@property (nonatomic, strong)   FBSDKLoginManager   *loginManager;
+
+@end
+
 @implementation ABLoginContext
+
+#pragma mark -
+#pragma mark Public Methods
+
+- (void)execute {
+    FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+    [login logInWithReadPermissions:@[@"public_profile", @"user_friends"]
+                 fromViewController:nil
+                            handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+                                if (error) {
+                                    NSLog(@"Process error");
+                                } else if (result.isCancelled) {
+                                    NSLog(@"Cancelled");
+                                } else {
+                                    NSLog(@"Logged in");
+                                }
+                            }];
+    self.loginManager = login;
+   }
+
+- (void)cancel {
+    [self.loginManager logOut];
+}
 
 @end
