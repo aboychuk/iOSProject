cbb7cb3eefc2106817417d42ec2a7e39541e2804
Andrew Boychuk <andrewboychuk@gmail.com>
2017-10-06 17:33:25 +0300

    ABUserDetailContext -> implementation added.

--- Source/Contexts/ABUserDetailContext/ABUserDetailContext.m
+++ Source/Contexts/ABUserDetailContext/ABUserDetailContext.m
@@ -8,6 +8,40 @@
 
 #import "ABUserDetailContext.h"
 
+#import "ABUser.h"
+
 @implementation ABUserDetailContext
+
+#pragma mark -
+#pragma mark Accessors
+
+- (ABUser *)user {
+    return self.model;
+}
+
+#pragma mark - 
+#pragma mark Public Methods
+
+- (void)execute {
+    NSDictionary *parameters = @{@"fields" : @"first_name,last_name,hometown,picture.type(large)"};
+    FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc] initWithGraphPath:self.model
+                                                                   parameters:parameters];
+    [request startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+        if (!error) {
+            [self parseResult:result];
+        }
+    }];
+}
+
+- (void)parseResult:(id)result {
+    NSURL *url = [NSURL URLWithString:[[[result valueForKey:@"picture"] valueForKey:@"data"] valueForKey:@"url"]];
+    
+    self.user.userID = [result valueForKey:@"userID"];
+    self.user.name = [result valueForKey:@"first_name"];
+    self.user.surname = [result valueForKey:@"last_name"];
+    self.user.hometown  = [result valueForKey:@"hometown"];
+    self.user.imageUrl = url;
+    self.user.state = ABModelDidLoad;
+}
 
 @end
