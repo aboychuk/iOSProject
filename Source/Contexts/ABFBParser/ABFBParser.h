7f52baffa1931d3813ce7d09f07e0f440ee78bb6
Andrew Boychuk <andrewboychuk@gmail.com>
2017-10-13 20:28:47 +0300

    ABFBParser added.

---
+++ Source/Contexts/ABFBParser/ABFBParser.h
@@ -0,0 +1,13 @@
+//
+//  ABFBParser.h
+//  iOSProject
+//
+//  Created by Andrew Boychuk on 10/13/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABFBParser : NSObject
+
+@end
---
+++ Source/Contexts/ABFBParser/ABFBParser.m
@@ -0,0 +1,13 @@
+//
+//  ABFBParser.m
+//  iOSProject
+//
+//  Created by Andrew Boychuk on 10/13/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "ABFBParser.h"
+
+@implementation ABFBParser
+
+@end
